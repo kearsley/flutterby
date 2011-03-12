63bfa9b3ae3dfad718db80add21679a298dccb3d
Kearsley Schieder-Wethy <turvity@gmail.com>
2011-03-12 16:11:14 +0100

    Finished re-organising for distutils.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 #This is a list of files to install, and where
 #(relative to the 'root' dir, where setup.py is)
 #You could be more specific.
-files = ["flutterby/resources/*"]
+files = [ "resources/img/*.png" ]
 
 setup(name = "flutterby",
       version = "0.2",
@@ -11,18 +11,23 @@ setup(name = "flutterby",
       author = "Kearsley Schieder-Wethy",
       author_email = "kearsley@frimble.net",
       url = "http://frimble.net/cms/flutterby",
+
+      # libraries = [ 'tweepy' ],
+
+      scripts = [ 'flutterby' ],
       
       #Name the folder where your packages live:
       #(If you have other packages (dirs) or modules (py files) then
       #put them into the package directory - they will be found 
       #recursively.)
-      packages = ['flutterby',],
+      packages = [ 'flutterby', ],
+      package_dir = { 'flutterby' : 'src/flutterby' },
       
       #'package' package must contain files (see list above)
       #I called the package 'package' thus cleverly confusing the whole issue...
       #This dict maps the package name =to=> directories
       #It says, package *needs* these files.
-      package_data = {'package' : files },
+      package_data = { 'flutterby' : files },
 
       long_description = """A Twitter client written in Python/PyGTK
 
